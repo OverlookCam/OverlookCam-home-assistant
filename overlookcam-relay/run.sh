b17e6c170d00ea1d2b172dcb76f4429b747d3c6a
rd3dprtllc <randy@rd-3d-printing.com>
2026-09-08 15:37:20 -0500

    Run manager behind production WSGI server

--- overlookcam-relay/run.sh
+++ overlookcam-relay/run.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 export OVERLOOKCAM_INSTALLATION_STATE="/data/installation.json"
 export OVERLOOKCAM_RELAY_PLATFORM="home-assistant"
-export OVERLOOKCAM_RELAY_VERSION="0.2.3"
+export OVERLOOKCAM_RELAY_VERSION="0.2.4"
 
 bashio::log.info "Starting OverlookCam Relay manager"
 cd /opt/overlookcam
-exec python3 manager.py
+exec python3 server.py
